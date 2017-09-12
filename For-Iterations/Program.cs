177a23afd6f25d410686722b4daf9171daf9413d
JackBurch <31357591+JackBurch@users.noreply.github.com>
2017-09-12 12:14:10 -0600

    InitalCommit
    two for iterations that count to 10, then 12

--- For-Iterations/Program.cs
+++ For-Iterations/Program.cs
@@ -10,6 +10,27 @@ namespace For_Iterations
     {
         static void Main(string[] args)
         {
+            //increase "i" by 1 10 times. 
+            // when i = 7, it prints found 7
+            for (int i = 0; i <10; i++)
+            {
+                Console.WriteLine(i.ToString());
+                // 
+                if(i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+
+            }
+            // myValue increases 12 times
+            
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);   
+            }
+            
+            Console.ReadLine();
         }
     }
 }
